060640d8f23ec1811b6e49bf112e6bc2a4d817af
BJustin07 <baquiranjustin07@gmail.com>
2024-09-27 15:31:38 +0800

    Learn: Dependency Injection and os.stdout and buffers.

---
+++ dependencyInjection/dependencyInjection_test.go
@@ -0,0 +1,16 @@
+package dependencyInjection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Justin")
+	got := buffer.String()
+	want := "Hello Justin"
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
